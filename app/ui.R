4514859bdd7f78bc48e2133d2a12d051addcdd2a
ltyue <tl2741@columbia.edu>
2017-04-26 12:33:58 -0400

    Add files via upload

--- app/ui.R
+++ app/ui.R
@@ -1,36 +1,134 @@
+
 library(shiny)
 
 
-
-color_select<-list(purple="purple",red="red")
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(navbarPage(
   
-  # Application title
-  titlePanel("我不知道我不知道我不知道看什么movie"),
+  ##link to css.file
+  theme = "bootstrap2.css",
+  
+  ##Project Title
+  "Movie Inside You",
+  
+  tabPanel("Home",
+           htmlOutput("blankspace"),
+           titlePanel("Adeventure in Movies"),
+           h4(htmlOutput("text")),
+           htmlOutput("teammates")
+  ),
   
   
-  sidebarLayout(
-    sidebarPanel(
-      width = 100,
-      actionButton("filter2_red", "red", 
-                   style="color: #fff; background-color: #f03b20"),
-      actionButton("filter2_purple", "purple", 
-                   style="color: #fff; background-color: #cf8cea"),
-      actionButton("filter2_pink", "pink", 
-                   style="color: #fff; background-color: #fa9fb5"),
-      actionButton("filter2_blue", "blue", 
-                   style="color: #fff; background-color: #3182bd"),
-      actionButton("filter2_yellow", "dark yellow", 
-                   style="color: #fff; background-color: #b8860b"),
-      actionButton("filter2_orange", "orange", 
-                   style="color: #fff; background-color: #fd8d3c")
-    ),
-    
-    
-    mainPanel(
-      h4("Our Recommendation"),dataTableOutput("result")
-    )
-  )
-))
+  ## 3D Globe tab
+  tabPanel("Mannual",
+           titlePanel("How to use the Application"),
+           absolutePanel(id = "controls", class = "panel panel-default",
+                         draggable = TRUE, 
+                         top = 180, left = 60, right = "auto", bottom = "auto",
+                         width = 350, height = "auto",
+                         
+                         h2("Explorer"),
+                         verbatimTextOutput('out6'),
+                         selectInput('in6', 'Search', state.name, multiple=TRUE, selectize=TRUE)
+           ),
+           absolutePanel(id = "controls", class = "panel panel-default",
+                          draggable = TRUE, 
+                          top = 180, left = 460, right = "auto", bottom = "auto",
+                          width = 350, height = "auto",
+             h2("Just Try Them"),
+             imageOutput("wordcloud1",width="100%",height="250px"),
+             imageOutput("wordcloud2",width="100%",height="250px"),
+             imageOutput("wordcloud3",width="100%",height="250px"),
+             imageOutput("wordcloud4",width="100%",height="250px")
+             ),
+           
+           
+           
+           globeOutput("Globe",width="100%",height="650px")),
+  
+  tabPanel("Recommend Me",
+           titlePanel("Coffee ,tea, and others traded between US and the world"),
+           absolutePanel(id = "controls", class = "panel panel-default",
+                         draggable = TRUE, 
+                         top = 180, left = 60, right = "auto", bottom = "auto",
+                         width = 350, height = "auto",
+                         
+                         h2("3D Explorer"),
+                         
+                         radioButtons(inputId = "type",
+                                      label  = "Choose import/export",
+                                      choices = c('Export','Import'),
+                                      selected ='Import'),
+                         sliderInput(inputId = "year_3D",
+                                     label = "Select a year",
+                                     value = 1996, min =1996, max =2016),
+                         sliderInput(inputId = "number_countries",
+                                     label = "Top Countries in Trade",
+                                     value = 10,min = 1,max = 50),
+                         selectInput(inputId = "commodity_3D",
+                                     label  = "Select the commodity",
+                                     choices = c('Annual Aggregate','Chocolate', 'Coffee','Cocoa','Spices','Tea'),
+                                     selected ='Coffee')
+           ),
+           
+           
+           
+           globeOutput("Globe",width="100%",height="650px")),
+  
+  
+  
+  tabPanel("I'm feeling Lucky!",
+           titlePanel("Coffee ,tea, and others traded between US and the world"),
+           absolutePanel(id = "controls", class = "panel panel-default",
+                         draggable = TRUE, 
+                         top = 180, left = 60, right = "auto", bottom = "auto",
+                         width = 350, height = "auto",
+                         
+                         h2("3D Explorer"),
+                         
+                         radioButtons(inputId = "type",
+                                      label  = "Choose import/export",
+                                      choices = c('Export','Import'),
+                                      selected ='Import'),
+                         sliderInput(inputId = "year_3D",
+                                     label = "Select a year",
+                                     value = 1996, min =1996, max =2016),
+                         sliderInput(inputId = "number_countries",
+                                     label = "Top Countries in Trade",
+                                     value = 10,min = 1,max = 50),
+                         selectInput(inputId = "commodity_3D",
+                                     label  = "Select the commodity",
+                                     choices = c('Annual Aggregate','Chocolate', 'Coffee','Cocoa','Spices','Tea'),
+                                     selected ='Coffee')
+           ),
+           
+           
+           
+           globeOutput("Globe",width="100%",height="650px")),
+  tabPanel("To be continued",
+           titlePanel("Coffee ,tea, and others traded between US and the world"),
+           absolutePanel(id = "controls", class = "panel panel-default",
+                         draggable = TRUE, 
+                         top = 180, left = 60, right = "auto", bottom = "auto",
+                         width = 350, height = "auto",
+                         
+                         h2("3D Explorer"),
+                         
+                         radioButtons(inputId = "type",
+                                      label  = "Choose import/export",
+                                      choices = c('Export','Import'),
+                                      selected ='Import'),
+                         sliderInput(inputId = "year_3D",
+                                     label = "Select a year",
+                                     value = 1996, min =1996, max =2016),
+                         sliderInput(inputId = "number_countries",
+                                     label = "Top Countries in Trade",
+                                     value = 10,min = 1,max = 50),
+                         selectInput(inputId = "commodity_3D",
+                                     label  = "Select the commodity",
+                                     choices = c('Annual Aggregate','Chocolate', 'Coffee','Cocoa','Spices','Tea'),
+                                     selected ='Coffee')
+           ),
+           
+           
+           
+           globeOutput("Globe",width="100%",height="650px"))))
